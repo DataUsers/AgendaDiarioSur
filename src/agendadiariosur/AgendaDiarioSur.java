ab853107689dd1b168718e2baebdcbf33ec6265b
luism <luism@LAPTOP-A2K9TQSK>
2017-03-31 20:06:40 +0200

    Test

--- src/agendadiariosur/AgendaDiarioSur.java
+++ src/agendadiariosur/AgendaDiarioSur.java
@@ -15,7 +15,7 @@ public class AgendaDiarioSur {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola Pablo");
     }
     
 }
