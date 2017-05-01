8befba82e684e24eb081a0256fae197f7e0e2990
DaryKiri <darykiri1@gmail.com>
2017-05-01 16:35:55 +0200

    añadido contador de visitas en entidad evento, casi implementada la bean de busqueda y pagina jsf de busqueda, refactorizado fichero js para compartir eventos

--- src/java/uma/informatica/sii/diarioSur/Evento.java
+++ src/java/uma/informatica/sii/diarioSur/Evento.java
@@ -28,11 +28,11 @@ import javax.persistence.OneToMany;
 public class Evento implements Serializable {
 
     public enum Tipo {
-        ACTOSPOLÍTICOS, CERTÁMENES,
-        CINES, CONCIERTOS, CONCURSOS, CONMMEMORACIONES, DEBATES, DESFILES,
-        ENTREGAPREMIOS, EVENTODEPORTIVO, EVENTOINFANTIL, EXPOSICIONES, FERIAS,
-        FIESTASBENÉFICAS, FIESTASNOCTURNAS, HOMENAJES, INAUGURACIONES, MUESTRASDEARTE, PRESENTACIONES,
-        AIRE_LIBRE, CAMPEONATOS, CLASESABIERTAS, CONFERENCIAS, CONGRESOS, GASTRONOMICO, MODA, MUSEOS, MUSICAL, TORNEOS
+	ACTOSPOLÍTICOS, CERTÁMENES,
+	CINES, CONCIERTOS, CONCURSOS, CONMMEMORACIONES, DEBATES, DESFILES,
+	ENTREGAPREMIOS, EVENTODEPORTIVO, EVENTOINFANTIL, EXPOSICIONES, FERIAS,
+	FIESTASBENÉFICAS, FIESTASNOCTURNAS, HOMENAJES, INAUGURACIONES, MUESTRASDEARTE, PRESENTACIONES,
+	AIRE_LIBRE, CAMPEONATOS, CLASESABIERTAS, CONFERENCIAS, CONGRESOS, GASTRONOMICO, MODA, MUSEOS, MUSICAL, TORNEOS
     }
     private static final long serialVersionUID = 1L;
     @Id
@@ -45,10 +45,11 @@ public class Evento implements Serializable {
     private String geolocalizacion;
     @Enumerated(EnumType.ORDINAL)
     private Tipo tipoEvento;
-    @ElementCollection (fetch = FetchType.EAGER)
+    @ElementCollection(fetch = FetchType.EAGER)
     private List<Date> fechas;
     private Time duracion;
     private Integer numero_entradas;
+    private Integer numeroVisitas;
     private String organizador;
     private Byte[][] imagenes;
     private String URLVideos;
@@ -58,151 +59,157 @@ public class Evento implements Serializable {
     @OneToMany
     private List<EntradaEvento> entradas;
 
-	public Integer getIdEvento() {
-		return idEvento;
-	}
+    public Integer getIdEvento() {
+	return idEvento;
+    }
 
-	public void setIdEvento(Integer idEvento) {
-		this.idEvento = idEvento;
-	}
+    public void setIdEvento(Integer idEvento) {
+	this.idEvento = idEvento;
+    }
 
-	public String getNombre() {
-		return nombre;
-	}
+    public String getNombre() {
+	return nombre;
+    }
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
+    public void setNombre(String nombre) {
+	this.nombre = nombre;
+    }
 
-	public String getDescripcion() {
-		return descripcion;
-	}
+    public String getDescripcion() {
+	return descripcion;
+    }
 
-	public void setDescripcion(String descripcion) {
-		this.descripcion = descripcion;
-	}
+    public void setDescripcion(String descripcion) {
+	this.descripcion = descripcion;
+    }
 
-	public Integer getPrecio() {
-		return precio;
-	}
+    public Integer getPrecio() {
+	return precio;
+    }
 
-	public void setPrecio(Integer precio) {
-		this.precio = precio;
-	}
+    public void setPrecio(Integer precio) {
+	this.precio = precio;
+    }
 
-	public String getGeolocalizacion() {
-		return geolocalizacion;
-	}
+    public String getGeolocalizacion() {
+	return geolocalizacion;
+    }
 
-	public void setGeolocalizacion(String geolocalizacion) {
-		this.geolocalizacion = geolocalizacion;
-	}
+    public void setGeolocalizacion(String geolocalizacion) {
+	this.geolocalizacion = geolocalizacion;
+    }
 
-	public Tipo getTipoEvento() {
-		return tipoEvento;
-	}
+    public Tipo getTipoEvento() {
+	return tipoEvento;
+    }
 
-	public void setTipoEvento(Tipo tipoEvento) {
-		this.tipoEvento = tipoEvento;
-	}
+    public void setTipoEvento(Tipo tipoEvento) {
+	this.tipoEvento = tipoEvento;
+    }
 
-	public List<Date> getFechas() {
-		return fechas;
-	}
+    public List<Date> getFechas() {
+	return fechas;
+    }
 
-	public void setFechas(List<Date> fechas) {
-		this.fechas = fechas;
-	}
+    public void setFechas(List<Date> fechas) {
+	this.fechas = fechas;
+    }
 
-	public Time getDuracion() {
-		return duracion;
-	}
+    public Time getDuracion() {
+	return duracion;
+    }
 
-	public void setDuracion(Time duracion) {
-		this.duracion = duracion;
-	}
+    public void setDuracion(Time duracion) {
+	this.duracion = duracion;
+    }
 
-	public Integer getNumero_entradas() {
-		return numero_entradas;
-	}
+    public Integer getNumero_entradas() {
+	return numero_entradas;
+    }
 
-	public void setNumero_entradas(Integer numero_entradas) {
-		this.numero_entradas = numero_entradas;
-	}
+    public void setNumero_entradas(Integer numero_entradas) {
+	this.numero_entradas = numero_entradas;
+    }
 
-	public String getOrganizador() {
-		return organizador;
-	}
+    public String getOrganizador() {
+	return organizador;
+    }
 
-	public void setOrganizador(String organizador) {
-		this.organizador = organizador;
-	}
+    public void setOrganizador(String organizador) {
+	this.organizador = organizador;
+    }
 
-	public Byte[][] getImagenes() {
-		return imagenes;
-	}
+    public Byte[][] getImagenes() {
+	return imagenes;
+    }
 
-	public void setImagenes(Byte[][] imagenes) {
-		this.imagenes = imagenes;
-	}
+    public void setImagenes(Byte[][] imagenes) {
+	this.imagenes = imagenes;
+    }
 
-	public String getURLVideos() {
-		return URLVideos;
-	}
+    public String getURLVideos() {
+	return URLVideos;
+    }
 
-	public void setURLVideos(String URLVideos) {
-		this.URLVideos = URLVideos;
-	}
+    public void setURLVideos(String URLVideos) {
+	this.URLVideos = URLVideos;
+    }
 
-	public String getURLOrganizador() {
-		return URLOrganizador;
-	}
+    public String getURLOrganizador() {
+	return URLOrganizador;
+    }
 
-	public void setURLOrganizador(String URLOrganizador) {
-		this.URLOrganizador = URLOrganizador;
-	}
+    public void setURLOrganizador(String URLOrganizador) {
+	this.URLOrganizador = URLOrganizador;
+    }
 
-	public List<CalificacionEvento> getCalificaciones() {
-		return calificaciones;
-	}
+    public List<CalificacionEvento> getCalificaciones() {
+	return calificaciones;
+    }
 
-	public void setCalificaciones(List<CalificacionEvento> calificaciones) {
-		this.calificaciones = calificaciones;
-	}
+    public void setCalificaciones(List<CalificacionEvento> calificaciones) {
+	this.calificaciones = calificaciones;
+    }
 
-	public List<EntradaEvento> getEntradas() {
-		return entradas;
-	}
+    public List<EntradaEvento> getEntradas() {
+	return entradas;
+    }
 
-	public void setEntradas(List<EntradaEvento> entradas) {
-		this.entradas = entradas;
-	}
+    public void setEntradas(List<EntradaEvento> entradas) {
+	this.entradas = entradas;
+    }
 
+    public Integer getNumeroVisitas() {
+	return numeroVisitas;
+    }
 
+    public void setNumeroVisitas(Integer numeroVisitas) {
+	this.numeroVisitas = numeroVisitas;
+    }
 
     @Override
     public int hashCode() {
-        int hash = 0;
-        hash += (idEvento != null ? idEvento.hashCode() : 0);
-        return hash;
+	int hash = 0;
+	hash += (idEvento != null ? idEvento.hashCode() : 0);
+	return hash;
     }
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Evento)) {
-            return false;
-        }
-        Evento other = (Evento) object;
-        if ((this.idEvento == null && other.idEvento != null) || (this.idEvento != null && !this.idEvento.equals(other.idEvento))) {
-            return false;
-        }
-        return true;
+	// TODO: Warning - this method won't work in the case the id fields are not set
+	if (!(object instanceof Evento)) {
+	    return false;
+	}
+	Evento other = (Evento) object;
+	if ((this.idEvento == null && other.idEvento != null) || (this.idEvento != null && !this.idEvento.equals(other.idEvento))) {
+	    return false;
+	}
+	return true;
     }
 
     @Override
     public String toString() {
-        return "uma.informatica.sii.diarioSur.Evento[ id=" + idEvento + " ]";
+	return "uma.informatica.sii.diarioSur.Evento[ id=" + idEvento + " ]";
     }
 
 }
--- src/java/uma/informatica/sii/diarioSur/beans/Busqueda.java
+++ src/java/uma/informatica/sii/diarioSur/beans/Busqueda.java
@@ -5,10 +5,14 @@
  */
 package uma.informatica.sii.diarioSur.beans;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import javax.inject.Named;
 import javax.enterprise.context.Dependent;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
 import uma.informatica.sii.diarioSur.Evento;
 
 /**
@@ -23,11 +27,91 @@ public class Busqueda {
     private List<String> filtros;
     private List<Evento> placeholderEvents;
     private List<Evento> eventosMostrar;
-    
+
     /**
      * Creates a new instance of Busqueda
      */
     public Busqueda() {
+	// Obtener query y etiqueta cookies
+	// Si no hay query ni filtro ni cookies, cargar etiquetas predeterminadas y filtrar predeterminado
+	// Si se ha hecho query, hacer query y filtrar mostrando los resultados
+	HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+	String query = request.getParameter("q");
+	String filtro = request.getParameter("filtrar");
+
+	// obtener cookies y asignar filtros
+	crearFiltroPredeterminado();
+	filtros = filtrosPredeterminados;
+
+	// Creacion de eventos placeholder
+	crearPlaceholder();
+
+	if (query != null && query.length() != 0) {
+	    System.out.println("Tiene query String: " + query);
+	    eventosMostrar = new ArrayList<>();
+	    Random rnd = new Random(System.currentTimeMillis());
+
+	    System.out.println("Numero de eventos placeholder: " + placeholderEvents.size());
+	    
+	    for (Evento evento : placeholderEvents) {
+		// busqueda normal por nombre, el query deberia de hacerce con el ejb
+		// Busqueda placeholder
+		if (filtro != null && filtro.length() != 0) {
+		    System.out.println("Tiene un filtro: " + filtro);
+		}
+		if (rnd.nextBoolean()) {
+		    eventosMostrar.add(evento);
+		}
+	    }
+	} else {
+	    eventosMostrar = placeholderEvents.subList(0, 5);
+	}
+    }
+
+    private void crearFiltroPredeterminado() {
+	filtrosPredeterminados = new ArrayList<>();
+	filtrosPredeterminados.add("Mas Visitados");
+	filtrosPredeterminados.add("Conciertos");
+	filtrosPredeterminados.add("Desfiles");
+	filtrosPredeterminados.add("Exposiciones");
+	filtrosPredeterminados.add("Ferias");
+	filtrosPredeterminados.add("Cines");
+    }
+
+    private void crearPlaceholder() {
+	Random rnd = new Random(System.currentTimeMillis());
+	placeholderEvents = new ArrayList<>();
+	
+	// Tipo de Eventos predeterminados para filtrar 
+	List<String> tipoEventos = new ArrayList<>();
+	tipoEventos.add(Evento.Tipo.CONCIERTOS.name());
+	tipoEventos.add(Evento.Tipo.DESFILES.name());
+	tipoEventos.add(Evento.Tipo.FERIAS.name());
+	tipoEventos.add(Evento.Tipo.EXPOSICIONES.name());
+	tipoEventos.add(Evento.Tipo.CINES.name());
+
+	for (int i = 0; i < 20; ++i) {
+	    Evento eventoPlaceholder = new Evento();
+	    eventoPlaceholder.setIdEvento(i);
+	    eventoPlaceholder.setNombre("Placeholder Nombre evento " + i);
+	    eventoPlaceholder.setPrecio(rnd.nextInt(100));
+	    eventoPlaceholder.setDescripcion("PLACEHOLDER description del evento" + i);
+	    eventoPlaceholder.setGeolocalizacion("36.714040, -4.433475");
+	    eventoPlaceholder.setOrganizador("OrganizadorNombre");
+	    eventoPlaceholder.setURLOrganizador("http://127.0.0.1:8080");
+	    eventoPlaceholder.setNumeroVisitas(rnd.nextInt(500));
+
+	    // Eleccion random de tipo de evento
+	    String tipoEventStr = tipoEventos.get(rnd.nextInt(tipoEventos.size()));
+	    Evento.Tipo tipoEvento = Evento.Tipo.valueOf(Evento.Tipo.class, tipoEventStr);
+	    eventoPlaceholder.setTipoEvento(tipoEvento);
+	    // imagenes
+	    placeholderEvents.add(eventoPlaceholder);
+	}
+    }
+
+    public String construirEnlace(Integer eventoId) {
+	return "evento.xhtml?evento=" + eventoId;
     }
 
     public List<String> getFiltros() {
@@ -45,7 +129,5 @@ public class Busqueda {
     public void setEventosMostrar(List<Evento> eventosMostrar) {
 	this.eventosMostrar = eventosMostrar;
     }
-    
-    
-    
+
 }
