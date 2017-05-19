a152c3d645864c36362066b0331a0932970a75c5
TLuigi003 <luis.m.96@hotmail.com>
2017-05-19 13:07:04 +0200

    Cambiado atributo imagenes en entidad evento

--- AgendaDiarioSurEE-ejb/src/java/uma/informatica/sii/diarioSur/entidades/Evento.java
+++ AgendaDiarioSurEE-ejb/src/java/uma/informatica/sii/diarioSur/entidades/Evento.java
@@ -28,11 +28,11 @@ import javax.persistence.OneToMany;
 public class Evento implements Serializable {
 
     public enum Tipo {
-	ACTOS_POLÍTICOS, CERTÁMENES,
-	CINES, CONCIERTOS, CONCURSOS, CONMMEMORACIONES, DEBATES, DESFILES,
-	ENTREGAPREMIOS, EVENTODEPORTIVO, EVENTOINFANTIL, EXPOSICIONES, FERIAS,
-	FIESTASBENÉFICAS, FIESTASNOCTURNAS, HOMENAJES, INAUGURACIONES, MUESTRAS_DE_ARTE, PRESENTACIONES,
-	AIRE_LIBRE, CAMPEONATOS, CLASES_ABIERTAS, CONFERENCIAS, CONGRESOS, GASTRONOMICO, MODA, MUSEOS, MUSICAL, TORNEOS
+        ACTOS_POLÍTICOS, CERTÁMENES,
+        CINES, CONCIERTOS, CONCURSOS, CONMMEMORACIONES, DEBATES, DESFILES,
+        ENTREGAPREMIOS, EVENTODEPORTIVO, EVENTOINFANTIL, EXPOSICIONES, FERIAS,
+        FIESTASBENÉFICAS, FIESTASNOCTURNAS, HOMENAJES, INAUGURACIONES, MUESTRAS_DE_ARTE, PRESENTACIONES,
+        AIRE_LIBRE, CAMPEONATOS, CLASES_ABIERTAS, CONFERENCIAS, CONGRESOS, GASTRONOMICO, MODA, MUSEOS, MUSICAL, TORNEOS
     };
     private static final long serialVersionUID = 1L;
     @Id
@@ -45,13 +45,13 @@ public class Evento implements Serializable {
     private String geolocalizacion;
     @Enumerated(EnumType.ORDINAL)
     private Tipo tipoEvento;
-    @ElementCollection (fetch =  FetchType.EAGER)
+    @ElementCollection(fetch = FetchType.EAGER)
     private List<Date> fechas;
     private Time duracion;
     private Integer numero_entradas;
     private Integer numeroVisitas;
     private String organizador;
-    private Byte[][] imagenes;
+    private String[] imagenes;
     private String URLVideos;
     private String URLOrganizador;
     @OneToMany
@@ -60,156 +60,156 @@ public class Evento implements Serializable {
     private List<EntradaEvento> entradas;
 
     public Integer getIdEvento() {
-	return idEvento;
+        return idEvento;
     }
 
     public void setIdEvento(Integer idEvento) {
-	this.idEvento = idEvento;
+        this.idEvento = idEvento;
     }
 
     public String getNombre() {
-	return nombre;
+        return nombre;
     }
 
     public void setNombre(String nombre) {
-	this.nombre = nombre;
+        this.nombre = nombre;
     }
 
     public String getDescripcion() {
-	return descripcion;
+        return descripcion;
     }
 
     public void setDescripcion(String descripcion) {
-	this.descripcion = descripcion;
+        this.descripcion = descripcion;
     }
 
     public Integer getPrecio() {
-	return precio;
+        return precio;
     }
 
     public void setPrecio(Integer precio) {
-	this.precio = precio;
+        this.precio = precio;
     }
 
     public String getGeolocalizacion() {
-	return geolocalizacion;
+        return geolocalizacion;
     }
 
     public void setGeolocalizacion(String geolocalizacion) {
-	this.geolocalizacion = geolocalizacion;
+        this.geolocalizacion = geolocalizacion;
     }
 
     public Tipo getTipoEvento() {
-	return tipoEvento;
+        return tipoEvento;
     }
 
     public void setTipoEvento(Tipo tipoEvento) {
-	this.tipoEvento = tipoEvento;
+        this.tipoEvento = tipoEvento;
     }
 
     public List<Date> getFechas() {
-	return fechas;
+        return fechas;
     }
 
     public void setFechas(List<Date> fechas) {
-	this.fechas = fechas;
+        this.fechas = fechas;
     }
 
     public Time getDuracion() {
-	return duracion;
+        return duracion;
     }
 
     public void setDuracion(Time duracion) {
-	this.duracion = duracion;
+        this.duracion = duracion;
     }
 
     public Integer getNumero_entradas() {
-	return numero_entradas;
+        return numero_entradas;
     }
 
     public void setNumero_entradas(Integer numero_entradas) {
-	this.numero_entradas = numero_entradas;
+        this.numero_entradas = numero_entradas;
     }
 
     public String getOrganizador() {
-	return organizador;
+        return organizador;
     }
 
     public void setOrganizador(String organizador) {
-	this.organizador = organizador;
+        this.organizador = organizador;
     }
 
-    public Byte[][] getImagenes() {
-	return imagenes;
+    public String[] getImagenes() {
+        return imagenes;
     }
 
-    public void setImagenes(Byte[][] imagenes) {
-	this.imagenes = imagenes;
+    public void setImagenes(String[] imagenes) {
+        this.imagenes = imagenes;
     }
 
     public String getURLVideos() {
-	return URLVideos;
+        return URLVideos;
     }
 
     public void setURLVideos(String URLVideos) {
-	this.URLVideos = URLVideos;
+        this.URLVideos = URLVideos;
     }
 
     public String getURLOrganizador() {
-	return URLOrganizador;
+        return URLOrganizador;
     }
 
     public void setURLOrganizador(String URLOrganizador) {
-	this.URLOrganizador = URLOrganizador;
+        this.URLOrganizador = URLOrganizador;
     }
 
     public List<CalificacionEvento> getCalificaciones() {
-	return calificaciones;
+        return calificaciones;
     }
 
     public void setCalificaciones(List<CalificacionEvento> calificaciones) {
-	this.calificaciones = calificaciones;
+        this.calificaciones = calificaciones;
     }
 
     public List<EntradaEvento> getEntradas() {
-	return entradas;
+        return entradas;
     }
 
     public void setEntradas(List<EntradaEvento> entradas) {
-	this.entradas = entradas;
+        this.entradas = entradas;
     }
 
     public Integer getNumeroVisitas() {
-	return numeroVisitas;
+        return numeroVisitas;
     }
 
     public void setNumeroVisitas(Integer numeroVisitas) {
-	this.numeroVisitas = numeroVisitas;
+        this.numeroVisitas = numeroVisitas;
     }
 
     @Override
     public int hashCode() {
-	int hash = 0;
-	hash += (idEvento != null ? idEvento.hashCode() : 0);
-	return hash;
+        int hash = 0;
+        hash += (idEvento != null ? idEvento.hashCode() : 0);
+        return hash;
     }
 
     @Override
     public boolean equals(Object object) {
-	// TODO: Warning - this method won't work in the case the id fields are not set
-	if (!(object instanceof Evento)) {
-	    return false;
-	}
-	Evento other = (Evento) object;
-	if ((this.idEvento == null && other.idEvento != null) || (this.idEvento != null && !this.idEvento.equals(other.idEvento))) {
-	    return false;
-	}
-	return true;
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Evento)) {
+            return false;
+        }
+        Evento other = (Evento) object;
+        if ((this.idEvento == null && other.idEvento != null) || (this.idEvento != null && !this.idEvento.equals(other.idEvento))) {
+            return false;
+        }
+        return true;
     }
 
     @Override
     public String toString() {
-	return "uma.informatica.sii.diarioSur.Evento[ id=" + idEvento + " ]";
+        return "uma.informatica.sii.diarioSur.Evento[ id=" + idEvento + " ]";
     }
 
 }
