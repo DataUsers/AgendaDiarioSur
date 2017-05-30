00b37382a70ab108c86ceb7627714c6099d5de7e
ismaelpineda <ismael.pineda@hotmail.com>
2017-05-30 22:57:01 +0200

    Perfil completamente arreglado

--- AgendaDiarioSurEE-war/src/java/uma/informatica/sii/diarioSur/beans/compraEventosBean.java
+++ AgendaDiarioSurEE-war/src/java/uma/informatica/sii/diarioSur/beans/compraEventosBean.java
@@ -9,7 +9,7 @@ import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
-import javax.inject.Inject;
+import javax.inject.Inject; 
 import javax.inject.Named;
 import javax.servlet.http.HttpServletRequest;
 import org.primefaces.model.map.DefaultMapModel;
