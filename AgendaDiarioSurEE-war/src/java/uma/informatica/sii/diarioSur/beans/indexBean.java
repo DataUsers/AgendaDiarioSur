f98f946f79e18105bddfbb176490b8daa376ce1e
pablo <ipvalders@me.com>
2017-05-31 02:57:32 +0200

    Rediseño de index, modificacion de plantilla, modificacion de css modificacion de indexBean

--- AgendaDiarioSurEE-war/src/java/uma/informatica/sii/diarioSur/beans/indexBean.java
+++ AgendaDiarioSurEE-war/src/java/uma/informatica/sii/diarioSur/beans/indexBean.java
@@ -8,9 +8,15 @@ package uma.informatica.sii.diarioSur.beans;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+import uma.informatica.sii.diarioSur.entidades.Evento;
+import uma.informatica.sii.diarioSur.negocio.DiarioSurException;
+import uma.informatica.sii.diarioSur.negocio.NegocioEvento;
 
 /**
  *
@@ -20,19 +26,32 @@ import javax.inject.Named;
 @RequestScoped
 public class indexBean {
     
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
-    private List<String> images;
-     
+    @EJB
+    private NegocioEvento negocio;
+    
+    
+    private List<Evento> eventos;
+    
     @PostConstruct
     public void init() {
-        images = new ArrayList<String>();
-        for (int i = 0; i <= 4; i++) {
-            images.add("image" + i + ".jpg");
+        
+        try {
+            eventos = negocio.obtenerEventos(7);
+            
+        } catch (DiarioSurException ex) {
+            Logger.getLogger(indexBean.class.getName()).log(Level.SEVERE, null, ex);
         }
+        
     }
  
-    public List<String> getImages() {
-        return images;
+
+    public List<Evento> getEventos() {
+        return eventos;
     }
+
+    public void setEventos(List<Evento> eventos) {
+        this.eventos = eventos;
+    }
+    
+    
 }
